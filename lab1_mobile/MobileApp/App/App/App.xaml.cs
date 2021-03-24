aa43b8e7222cbd4a5b81bed86ac10f7ac2bd6364
Mikołaj Czubak <mikolaj.czubak@crafton.pl>
2021-03-24 16:36:10 +0100

    app

---
+++ lab1_mobile/MobileApp/App/App/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App
+{
+    public partial class App : Application
+    {
+        public static object PhonewordTranslator { get; internal set; }
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
